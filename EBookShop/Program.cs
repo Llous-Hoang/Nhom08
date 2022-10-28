1835080238328dc44465112f5faebf7d6a0afc80
Llous-Hoang <lloushoang@gmail.com>
2022-10-28 13:17:59 +0700

    Builder, Add <Session>

--- EBookShop/Program.cs
+++ EBookShop/Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "MySession";
+    options.IdleTimeout=new TimeSpan(15,0,0,0);  
+});
 
 var app = builder.Build();
 
