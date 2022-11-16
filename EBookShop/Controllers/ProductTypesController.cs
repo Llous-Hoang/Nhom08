b7c496583ace4952e9d830ca6917089e5ac28206
Llous-Hoang <lloushoang@gmail.com>
2022-11-16 16:11:09 +0700

    Full_View & ProductTypesController

--- EBookShop/Controllers/ProductTypesController.cs
+++ EBookShop/Controllers/ProductTypesController.cs
@@ -1,19 +1,150 @@
 ﻿using EBookShop.Data;
+using EBookShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EBookShop.Controllers
 {
     public class ProductTypesController : Controller
     {
-        private EBookShopContext _context;
+        private readonly EBookShopContext _context;
+
         public ProductTypesController(EBookShopContext context)
         {
             _context = context;
         }
 
-        public IActionResult Index()
+        // GET: ProductTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ProductTypes.ToListAsync());
+        }
+
+        // GET: ProductTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ProductTypes == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Status")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Edit/Id
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ProductTypes == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Status")] ProductType productType)
+        {
+            if (id != productType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductTypeExists(productType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Delete/Id
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ProductTypes == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        // POST: ProductTypes/Delete/Id
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ProductTypes == null)
+            {
+                return Problem("Entity set 'EshopContext.ProductTypes'  is null.");
+            }
+            var productType = await _context.ProductTypes.FindAsync(id);
+            if (productType != null)
+            {
+                _context.ProductTypes.Remove(productType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            return _context.ProductTypes.Any(e => e.Id == id);
         }
     }
 }
