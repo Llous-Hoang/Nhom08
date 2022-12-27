a0399bdb96e0ee989180953e46d25a2a79b14070
Llous-Hoang <lloushoang@gmail.com>
2022-12-27 13:18:57 +0700

    fix #27/12

---
+++ EBookShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using EBookShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoginController : Controller
+    {
+        private EBookShopContext _context;
+        private IWebHostEnvironment _environment;
+        public LoginController(EBookShopContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var account = _context.Accounts.FirstOrDefault(a => a.username == username && a.password == password);
+            if (account != null)
+            {
+                HttpContext.Session.SetString("username", username);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMsg = "Login failed!";
+                return View();
+            }
+        }
+    }
+}
